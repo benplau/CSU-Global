912fd9eccc7a60b6f16b32856e00525db4169a02
Benjamin Lau <benbenpeacelau@gmail.com>
2024-05-03 19:14:20 +0800

    CT3

---
+++ 24SB_CSC450_CPP/src/m3/CSC450_CT3.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int num1, num2, num3;
+//
+//	cout << "Enter three integer values:" << endl;
+//
+//	cout << "num1:";
+//	cin >> num1;
+//
+//	cout << "num2:";
+//	cin >> num2;
+//
+//	cout << "num3:";
+//	cin >> num3;
+//
+//	int *ptr1 = new int;
+//	int *ptr2 = new int;
+//	int *ptr3 = new int;
+//
+//	*ptr1 = num1;
+//	*ptr2 = num2;
+//	*ptr3 = num3;
+//
+//	cout << "\nVariable contents:\n";
+//	cout << "num1 = " << num1 << ", ptr1 = " << *ptr1 << endl;
+//	cout << "num2 = " << num2 << ", ptr2 = " << *ptr2 << endl;
+//	cout << "num3 = " << num3 << ", ptr3 = " << *ptr3 << endl;
+//
+//	delete ptr1;
+//	delete ptr2;
+//	delete ptr3;
+//
+//	return 0;
+//}
